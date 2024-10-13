bdd9cf221edf39fb38acd5d3898d7f425158522c
Anthony eberechukwu Ochuba <anthonyeberechukwuochuba@Anthonys-MBP.fritz.box>
2024-10-13 20:49:39 +0200

    pushing codes

--- obj/Release/net8.0/SadiqWebApp.AssemblyInfo.cs
+++ obj/Release/net8.0/SadiqWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SadiqWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d4a18c5550bc1f53c8dd1ce5350daf0d9816a5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c3e960bb91c3af2a4f7c56e19a3faa905fcbe01")]
 [assembly: System.Reflection.AssemblyProductAttribute("SadiqWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SadiqWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
